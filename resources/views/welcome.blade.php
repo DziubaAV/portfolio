65107b0fc204177450eb9b7d6a46ab28590c74a0
DziubaAV <dz.artsiomi@gmail.com>
2023-04-02 19:42:43 +0300

    Подкл. шрифтов

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,8 @@
         <!-- Google Fonts - Krona One -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Roboto:wght@100&display=swap" rel="stylesheet">
+
         
 
         <!-- Styles -->
